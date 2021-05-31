3c54798d730b1e381ba0cba71ba33b49291badc7
Yusuf Abdulkarim <haywhyze@gmail.com>
2021-05-31 05:29:59 +0100

    last update

--- src/components/Modals/addProductModal/index.js
+++ src/components/Modals/addProductModal/index.js
@@ -211,8 +211,10 @@ const Modal = ({ selectedProduct, setSelectedProduct }) => {
   const [enableThirdContinueBtn, setThirdContinueBtn] = useState(false);
   const [enableFourthContinueBtn, setFourthContinueBtn] = useState(false);
   const [enableFifthContinueBtn, setFifthContinueBtn] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const submitSellerProduct = async () => {
+    setLoading(true);
     const {
       main_image,
       other_product_img_1,
@@ -268,6 +270,7 @@ const Modal = ({ selectedProduct, setSelectedProduct }) => {
           Authorization: `Bearer ${localStorage.getItem('safely_buy_token')}`,
         },
       });
+      setLoading(false);
       history.push({
         pathname: '/success-error',
         state: {
@@ -283,7 +286,6 @@ const Modal = ({ selectedProduct, setSelectedProduct }) => {
       });
     }
   };
-
   const { category_id, subcategory_id, condition, brand } = state;
 
   const subCategoryObj = {
@@ -392,46 +394,69 @@ const Modal = ({ selectedProduct, setSelectedProduct }) => {
               {step === 5 ? 'Product Details' : 'Add a new product'}
             </h3>
 
-            {step === 0 && (
+            {!loading && step === 0 && (
               <ModalButton
                 able={subcategory_id && condition && brand}
                 handler={() => setSteps(1)}
               />
             )}
 
-            {step === 1 && (
+            {!loading && step === 1 && (
               <ModalButton
                 able={enableFirstContinueBtn}
                 handler={() => setSteps(2)}
               />
             )}
 
-            {step === 2 && (
+            {!loading && step === 2 && (
               <ModalButton
                 able={enableSecondContinueBtn}
                 handler={() => setSteps(3)}
               />
             )}
 
-            {step === 3 && (
+            {!loading && step === 3 && (
               <ModalButton
                 able={enableThirdContinueBtn}
                 handler={() => setSteps(4)}
               />
             )}
 
-            {step === 4 && (
+            {!loading && step === 4 && (
               <ModalButton
                 able={enableFourthContinueBtn}
                 handler={() => setSteps(5)}
               />
             )}
 
-            {step === 5 && (
+            {!loading && step === 5 && (
               <ModalButton
                 able={enableFifthContinueBtn}
                 handler={submitSellerProduct}
               />
+            )}
+
+            {loading && (
+              <svg
+                className='animate-spin -ml-1 mr-3 h-5 w-5 text-purple-500'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+              >
+                <circle
+                  className='opacity-25'
+                  cx='12'
+                  cy='12'
+                  r='10'
+                  stroke='currentColor'
+                  strokeWidth='4'
+                ></circle>
+                <path
+                  className='opacity-75'
+                  fill='currentColor'
+                  d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                ></path>
+              </svg>
             )}
 
             <span
